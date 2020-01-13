0ba985d03926c91cc60d4b3c3799a545d67442f6
ningzhenyu <zhenyu_ning@qingsongchou.com>
2020-01-13 10:04:07 +0800

    更新一下

--- src/plugins/postion_fix.js
+++ src/plugins/postion_fix.js
@@ -3,26 +3,25 @@ const pos_fix = {};
 pos_fix.install = function(Vue,options){
     let pos_fix_time_handle = null;
 
-    document.body.addEventListener('focus',function(e){
-        const n = e.target.nodeName.toLowerCase()
-        if( n == 'input' || n == 'textarea'){
-            clearTimeout(pos_fix_time_handle)
-        }
-    },true)
-
     Vue.directive('pos_fix',{
         bind:function(el){
            el.addEventListener('blur',function(){
                 pos_fix_time_handle = setTimeout(function(){
-                    if(document.activeElement == document.body){
-                        const h = document.body.scrollTop || document.documentElement.scrollTop;
-                        window.scrollTo(0, h+1);
-                    }
+                  scroll();
                 },200)
-            })
+            });
+            el.addEventListener('focus',function(){
+              clearTimeout(pos_fix_time_handle)
+          });
         }
     })
-    
+}
+
+function scroll(){
+  if(document.activeElement == document.body){
+    const h = document.body.scrollTop || document.documentElement.scrollTop;
+    window.scrollTo(0, h+1);
+}
 }
 
 export default pos_fix
